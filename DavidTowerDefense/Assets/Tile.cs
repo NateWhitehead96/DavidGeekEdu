077cc5d60091de88c0ceb78a969c86b9d37a44e1
NateWhitehead96 <50887818+NateWhitehead96@users.noreply.github.com>
2022-05-20 20:36:10 -0400

    Started our tower defense game

---
+++ DavidTowerDefense/Assets/Tile.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public bool isOccupied; // true or false to know if this tile is occupied
+    
+}
